a092a7f134d73b3bb591a84ee021cff37ac2be8d
hp15aug <hpaneru04@gmail.com>
2025-12-03 00:18:27 +0530

    Solved TrailingZeros

---
+++ src/Introductory/TrailingZeros.java
@@ -0,0 +1,57 @@
+//package Introductory;
+
+import java.util.*;
+import java.io.*;
+
+public class TrailingZeros {
+static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+static StringTokenizer st;
+static PrintWriter out = new PrintWriter(System.out);
+
+public static void main(String[] args) throws Exception {
+    int t = 1;
+    // t = nextInt(); // Uncomment for multiple test cases
+
+    while(t-- > 0) {
+        solve();
+    }
+
+    out.close();
+}
+
+static void solve() throws Exception {
+    // Your solution here
+    int n = nextInt();
+    int c=0;
+
+    while(n>0){
+        n/=5;
+        c+=n;
+    }
+    out.println(c);
+}
+
+// Fast I/O methods
+static String next() throws Exception {
+    while(st == null || !st.hasMoreElements()) {
+        st = new StringTokenizer(br.readLine());
+    }
+    return st.nextToken();
+}
+
+static int nextInt() throws Exception {
+    return Integer.parseInt(next());
+}
+
+static long nextLong() throws Exception {
+    return Long.parseLong(next());
+}
+
+static double nextDouble() throws Exception {
+    return Double.parseDouble(next());
+}
+
+static String nextLine() throws Exception {
+    return br.readLine();
+}
+}
